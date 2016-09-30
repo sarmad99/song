c37fe4ce9bd170334a73c666489fb463bf8db918
sarmad99 <sarmadyousif19@gmail.com>
2016-09-29 20:44:06 -0400

    added the beer song class

---
+++ BeerSong.php
@@ -0,0 +1,34 @@
+<?php
+class BeerSong{
+	
+	public function main(){
+		
+		
+		
+	}
+	
+	public function Ninety_Nine_Bottles_of_Beer(){
+		
+		echo "99	bottles	of	beer	on	the	wall,	99	bottles	of	beer \n
+			take	one	down,	pass	it	around,	98	bottles	of	beer	on	the	wall.\n
+			98	bottles	of	beer	on	the	wall,	98	bottles	of	beer \n
+			take	one	down,	pass	it	around,	97	bottles	of	beer	on	the	wall.\n
+			97	bottles	of	beer	on	the	wall,	97	bottles	of	beer\n
+			take	one	down,	pass	it	around,	96	bottles	of	beer	on	the	wall.\n
+			...\n
+			2	bottles	of	beer	on	the	wall,	2	bottles	of	beer\n
+			take	one	down,	pass	it	around,	1	bottle	of	beer	on	the	wall.\n
+			1	bottle	of	beer	on	the	wall,	1	bottle	of	beer\n
+			take	one	down,	pass	it	around,	no	more	bottles	of	beer	on	the	wall."
+		
+	}
+	
+	
+	
+}
+
+
+
+
+
+?>
